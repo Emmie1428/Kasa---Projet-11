fd02ac6d9c4456f70c82091479ac06e22e5edf24
Emmie Gemme <mimiemmie@gmail.com>
2025-11-30 12:09:56 -0500

    Changement noms dossiers

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,12 @@ function App() {
 	
 	return (
 		<div>
+		
 			
 			<h1>App</h1>
+		
+			
+
 
 		</div>
 	)
--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,22 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import App from './App'
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
-import Accueil from "./Page/Accueil/Accueil"
 import APropos from './Page/APropos/APropos'
+import Accueil from './Page/Accueil/Accueil.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path="/" element={<App />}/>
-        <Route path="/Accueil" element={<Accueil />}/>
-        <Route path="/APropos" element={<APropos />}/>
-        <Route path="*" element={<Error />}/>
+        <Route path="/" element={<App />} />
+        <Route path="/accueil" element={<Accueil />} />
+        <Route path="/apropos" element={<APropos />} />
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </BrowserRouter>
   </StrictMode>
 )
+
+export default createRoot
