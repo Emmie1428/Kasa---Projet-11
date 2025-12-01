902b6cff28e2620146b7d6d03a542c67a4922c9e
Emmie Gemme <mimiemmie@gmail.com>
2025-11-30 19:55:17 -0500

    Component Footer terminé

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,12 @@
+import logoBlanc from "../../Assests/logoBlanc.png"
+import "./Footer.scss"
+
 function Footer () {
-    return ("Footer")
+    return (<div className="footer">
+                <img src={logoBlanc} alt="Logo Kasa" />
+                <h2>© 2020 Kasa. All rights reserved</h2>
+            </div>
+            )
 }
 
 export default Footer
