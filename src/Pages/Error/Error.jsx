466a5a17ba8d6e34c99d4481585bc1e4f77b0962
Emmie Gemme <mimiemmie@gmail.com>
2025-12-01 15:12:23 -0500

    Component Error terminé

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,5 +1,13 @@
+import {NavLink} from "react-router-dom"
+import "./Error.scss"
+import error404 from "../../Assests/error404.png"
+
 function Error () {
-    return ("error")
+    return (<div className="error">
+        <img src={error404} alt="Erreur 404" />
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        <NavLink to="/" className="error_link">Retouner sur la page d'accueil</NavLink>
+    </div>)
 }
 
 export default Error
