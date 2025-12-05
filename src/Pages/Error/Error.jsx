2059c6051481ee93e7f1a176d33106458108f38f
Emmie Gemme <mimiemmie@gmail.com>
2025-12-04 21:37:24 -0500

    Feat page Error scss format mobile terminé

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -4,8 +4,8 @@ import error404 from "../../Assests/error404.png"
 
 function Error () {
     return (<div className="error">
-        <img src={error404} alt="Erreur 404" />
-        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        <img src={error404} alt="Erreur 404" className="error_404"/>
+        <h2 className="error_texte">Oups! La page que <br className="error_texte_break"/>vous demandez n'existe pas.</h2>
         <NavLink to="/" className="error_link">Retouner sur la page d'accueil</NavLink>
     </div>)
 }
