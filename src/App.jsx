077685f92c8f8207485e781fe1657e993ad4f625
Emmie Gemme <mimiemmie@gmail.com>
2025-12-10 10:28:23 -0500

    Docs Retrait lien vers dossier supprimé

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from 'react-router-dom'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 import Router from './Components/Router'
-import './App.scss'
 
 function App() {
   return (<div className="app">
